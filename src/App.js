99846ce509f459f2a60e03a0d252b4e8752a4f81
Emre <48946077+FuzEmo@users.noreply.github.com>
2020-03-23 19:04:53 +0300

    Card

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {FootballCard} from "./components/FootballCard";
+
+function App() {
+  return (
+    
+
+
+    <div className="App">
+ 
+      <FootballCard/>   
+
+    </div>
+
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/FootballCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export const FootballCard = () => {
+    return (
+     <div className="card-main">
+
+            <div className="player-image"/>
+            <div className="team-image"/>
+
+     </div>
+
+            
+        
+    );
+}
