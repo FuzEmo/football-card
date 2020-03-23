83a2f73fd3aa9d3d69f576a7b2531376a4ce1377
Emre <48946077+FuzEmo@users.noreply.github.com>
2020-03-23 19:09:29 +0300

    Card Animation

--- src/components/FootballCard.js
+++ src/components/FootballCard.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-
 export const FootballCard = () => {
     return (
      <div className="card-main">
-
+       <div className="card-animation">
+        <div className="card-front">
             <div className="player-image"/>
             <div className="team-image"/>
-
+        </div>
+        <div className="card-back">
+            <div>Stats</div>
+        </div>
+       </div>
      </div>
 
             
