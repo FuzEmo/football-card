7fa249cd4a1277eba51b5c2fa38e32580a1383dd
Emre <48946077+FuzEmo@users.noreply.github.com>
2020-04-01 04:43:17 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import './App.css';
 import {FootballCard} from "./components/FootballCard";
-import $ from "jquery";
+
+
 function App() {
- 
   
   return (
     
-
-
     <div className="App">
 
       <div className="players-layout">
       <FootballCard/>  
       </div>
-  
+
     </div>
-
-
- 
   );
 }
 
--- src/components/FootballCard.js
+++ src/components/FootballCard.js
@@ -7,8 +7,6 @@ import $ from "jquery";
 
 export const FootballCard = () => {
   
-
-
   useEffect (() =>{
     $('.flip').click(function() {
       $(this).find('.card').addClass('flipped').mouseleave(function() {
@@ -17,11 +15,7 @@ export const FootballCard = () => {
       return true;
     });
   })
-
- 
-
     const players = useFetchPlayers();
-
         const Players = players.players;
         console.log(Players);
 
@@ -34,13 +28,46 @@ export const FootballCard = () => {
                 <img class="player-image"
                      alt='player'
                      src={player.picture}></img>
-            </div>
+                 </div>
             <div class="team-image"/>
-            <div class=" face back">  
-               <div>{player.surname}</div>
-         <div>{player.name}</div>
+            <div class=" face back"> 
 
-               
+            <div class="player-name">{player.name}</div>
+            <div class="player-surname">{player.surname}</div>
+          
+                <div class="stats">
+                    <div class="matches">{player.matches}
+                    <br/>
+                    <a>Matches</a>
+                    </div>
+                    <div class="goals">{player.goals}
+                    <br/>
+                    <a>Goals</a>
+                    </div>
+                    <div class="asists">{player.asists}
+                    <br/>
+                    <a>Asists</a>
+                    </div>
+                <div class="player-info">
+                    <div class="player-position">
+                      <a>Position</a>
+                      <br/>
+                      {player.position}</div> 
+                    <div className="flag-container">    
+                <img
+                    class="flag"
+                    src={player.flag}
+                     alt='flag'
+                    ></img>
+                 </div> 
+                   <div class="nationality">
+                     {player.nationality} 
+                     </div> 
+                 <div class="value">
+                    <h1>{player.value} € </h1>
+                  </div>  
+                </div>
+                </div>
             </div>
            </div>
         </div>
